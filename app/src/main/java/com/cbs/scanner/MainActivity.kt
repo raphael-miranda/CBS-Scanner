033b6c34452b8045f56cfdde9c4597f9e1d242ac
raphael <raphaelmiranda.l9v@gmail.com>
2025-01-25 16:41:20 +0800

    completed scan qr code and barcode.

--- app/src/main/java/com/cbs/scanner/MainActivity.kt
+++ app/src/main/java/com/cbs/scanner/MainActivity.kt
@@ -1,8 +1,8 @@
 package com.cbs.scanner
 
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
+import android.widget.CheckBox
 import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
@@ -10,12 +10,17 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.slider.Slider
 import com.journeyapps.barcodescanner.ScanContract
 import com.journeyapps.barcodescanner.ScanIntentResult
 import com.journeyapps.barcodescanner.ScanOptions
+import java.util.Locale
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var sliderSelector: Slider
+    private lateinit var chkManual: CheckBox
 
     private lateinit var txtRunningNr: TextView
     private lateinit var txtLocation: TextView
@@ -24,25 +29,41 @@ class MainActivity : AppCompatActivity() {
     private lateinit var txtDNr: TextView
     private lateinit var txtQuantity: TextView
 
+    private var runningNumber: Int = 0
+
+
     private val barcodeLauncher = registerForActivityResult<ScanOptions, ScanIntentResult>(
         ScanContract()
     ) { result: ScanIntentResult ->
         if (result.contents == null) {
             Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show()
         } else {
-            Toast.makeText(
-                this,
-                "Scanned: " + result.contents,
-                Toast.LENGTH_LONG
-            ).show()
-            Log.d("===========", result.contents)
+            txtLocation.text = "PA"
+            txtCartonNr.text = result.contents
+            txtPartNr.text = ""
+            txtDNr.text = ""
+            txtQuantity.text = ""
 
+            runningNumber += 1
+            txtRunningNr.text = String.format(Locale.getDefault(), "%d", runningNumber)
+        }
+    }
+
+    private val qrCodeLauncher = registerForActivityResult<ScanOptions, ScanIntentResult>(
+        ScanContract()
+    ) { result: ScanIntentResult ->
+        if (result.contents == null) {
+            Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show()
+        } else {
             val parseData = result.contents.split(";")
-            txtLocation.setText("PA")
-            txtCartonNr.setText(parseData[0])
-            txtPartNr.setText(parseData[1])
-            txtDNr.setText(parseData[2])
-            txtQuantity.setText(parseData[3])
+            txtLocation.text = "PA"
+            txtCartonNr.text = parseData[0]
+            txtPartNr.text = parseData[1]
+            txtDNr.text = parseData[2]
+            txtQuantity.text = parseData[3]
+
+            runningNumber += 1
+            txtRunningNr.text = String.format(Locale.getDefault(), "%d", runningNumber)
         }
     }
 
@@ -56,6 +77,7 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        sliderSelector = findViewById(R.id.sliderSelector)
         txtRunningNr = findViewById(R.id.txtRunning)
         txtLocation = findViewById(R.id.txtLocation)
         txtPartNr = findViewById(R.id.txtPart)
@@ -68,6 +90,23 @@ class MainActivity : AppCompatActivity() {
 
         }
 
+        chkManual = findViewById(R.id.chkManual)
+        chkManual.setOnCheckedChangeListener { button, result ->
+            if (result) {
+                txtLocation.isEnabled = true
+                txtPartNr.isEnabled = true
+                txtCartonNr.isEnabled = true
+                txtDNr.isEnabled = true
+                txtQuantity.isEnabled = true
+            } else {
+                txtLocation.isEnabled = false
+                txtPartNr.isEnabled = false
+                txtCartonNr.isEnabled = false
+                txtDNr.isEnabled = false
+                txtQuantity.isEnabled = false
+            }
+        }
+
         val btnScan : Button = findViewById(R.id.btnScan)
         btnScan.setOnClickListener {
             scan()
@@ -76,13 +115,34 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun scan() {
+
         val options = ScanOptions()
-        options.setDesiredBarcodeFormats(ScanOptions.ALL_CODE_TYPES)
-        options.setPrompt("Scan a barcode")
+        options.setPrompt("Scan a code")
         options.setCameraId(0) // Use a specific camera of the device
         options.setBeepEnabled(true)
         options.setBarcodeImageEnabled(true)
-        barcodeLauncher.launch(options)
+        options.setOrientationLocked(true)
+        options.setCaptureActivity(PortraitCaptureActivity::class.java)
+
+        val sliderValue = sliderSelector.value
+        if (sliderValue >= 1.5f) {
+            options.setDesiredBarcodeFormats(ScanOptions.QR_CODE)
+            qrCodeLauncher.launch(options)
+        } else {
+            options.setDesiredBarcodeFormats(
+                ScanOptions.CODE_128,
+                ScanOptions.CODE_39,
+                ScanOptions.CODE_93,
+                ScanOptions.EAN_8,
+                ScanOptions.EAN_13,
+                ScanOptions.ITF,
+                ScanOptions.UPC_A,
+                ScanOptions.UPC_E,
+                ScanOptions.PDF_417,
+                ScanOptions.DATA_MATRIX
+            )
+            barcodeLauncher.launch(options)
+        }
     }
 
 
---
+++ app/src/main/java/com/cbs/scanner/PortraitCaptureActivity.kt
@@ -0,0 +1,5 @@
+package com.cbs.scanner
+
+import com.journeyapps.barcodescanner.CaptureActivity
+
+class PortraitCaptureActivity: CaptureActivity()
